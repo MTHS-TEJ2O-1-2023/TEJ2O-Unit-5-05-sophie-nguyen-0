b4ff9cb99ab0951e102ebb9aa3098678e6dc517e
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-13 17:28:07 +0000

    python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,21 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: sophie
+Created on: oct 2023
+This module is a Micro:bit MicroPython program does traffic light
 """
 
 from microbit import *
 
+# variables
+neopixel_strip = neopixel.NeoPixel(pin16, 8)
 
-display.scroll("Hello, World!")
+display.clear
+display.show(Image.HEART)
+sleep(1000)
+
+while True:
+np[0] = (255, 0, 0)
+
+    if button_a.is_pressed():
+
+
