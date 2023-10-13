62616a5e71561ddb254c0915b15bf9397b088b77
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-13 12:45:12 -0400

    readme

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: sophie
+ * Created on: oct 2023
+ * This program is a traffic light
 */
 
 basic.showString('Hello, World!')
