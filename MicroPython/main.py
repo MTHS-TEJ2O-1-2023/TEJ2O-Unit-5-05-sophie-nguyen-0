76a03c479dd540c6d4949ca739ca96c7b1e8dfd0
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-13 18:16:42 +0000

    python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,7 @@ This module is a Micro:bit MicroPython program does traffic light
 """
 
 from microbit import *
+import neopixel
 
 # variables
 neopixel_strip = neopixel.NeoPixel(pin16, 8)
@@ -14,8 +15,26 @@ display.show(Image.HEART)
 sleep(1000)
 
 while True:
-np[0] = (255, 0, 0)
+    neopixel_strip[0] = (0, 0, 0)
+    neopixel_strip[1] = (0, 0, 0)
+    neopixel_strip[2] = (0, 0, 0)
+    print(neopixel_strip[0])
+    print(neopixel_strip[1])
+    print(neopixel_strip[2])
 
     if button_a.is_pressed():
-
-
+        neopixel_strip[0] = (0, 255, 0)
+        print(neopixel_strip[0])
+        sleep(3000)
+        neopixel_strip[0] = (0, 0, 0)
+        neopixel_strip[1] = (255, 255, 0)
+        print(neopixel_strip[0])
+        print(neopixel_strip[1])
+        sleep(3000)
+        neopixel_strip[1] = (0, 0, 0)
+        neopixel_strip[2] = (255, 0, 0)
+        print(neopixel_strip[1])
+        print(neopixel_strip[2])
+        sleep(3000)
+        neopixel_strip[2] = (0, 0, 0)
+        print(neopixel_strip[2])
